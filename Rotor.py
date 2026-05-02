14c836f8818ddbd3a0029f22818d545ede09dd99
Souvik Basak <souvik.b416@gmail.com>
2026-05-01 20:34:53 -0400

    testing

--- Rotor.py
+++ Rotor.py
@@ -1,15 +1,21 @@
+from util import *
+import json
+
 class Rotor:
-    def __init__(self, wiring, ring_setting, notch_letter, next_rotor):
+    def __init__(self, wiring, ring_setting, notch_letters, next_rotor):
         self.wiring = [0] * 26
         for i in range(len(wiring)):
             self.wiring[i] = num(wiring[i])
-
         self.inverse_wiring = [0] * 26
         for i in range(26):
             self.inverse_wiring[self.wiring[i]] = i
+
+        self.notch_letters = []
+        for letter in notch_letters:
+            self.notch_letters.append(num(letter))
+
         self.offset = 0
         self.ring_setting = ring_setting
-        self.notch_letter = num(notch_letter)
         self.next_rotor = next_rotor
 
     #Takes letter as number input
@@ -24,34 +30,27 @@ class Rotor:
         return (wired - self.offset + self.ring_setting) % 26
     
     def step(self):
-        if self.offset == self.notch_letter and self.next_rotor != None:
+        if self.offset in self.notch_letters and self.next_rotor != None:
             self.next_rotor.step()
         self.offset = (self.offset + 1) % 26
 
-    
-wiring1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
-wiring2 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
-wiring3 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
-reflect = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
+rotorNum = ["III", "III", "VIII"]
+reflectorName = "UKW-B"
+ringSettings = [0, 0, 0]
 
-def letter(index):
-    return chr(index+ord('A'))
+with open("wirings.json") as file:
+    data = json.load(file)
+    rotors = data["rotors"]
+    reflectors = data["reflectors"]
 
-def is_letter(char):
-    return (char >= 'A' and char <= 'Z') or (char >= 'a' and char <= 'z')
+    Rotor1 = Rotor(rotors[rotorNum[0]]["wiring"], ringSettings[0], rotors[rotorNum[0]]["notch"], None)
+    Rotor2 = Rotor(rotors[rotorNum[1]]["wiring"], ringSettings[1], rotors[rotorNum[1]]["notch"], Rotor1)
+    Rotor3 = Rotor(rotors[rotorNum[2]]["wiring"], ringSettings[2], rotors[rotorNum[2]]["notch"], Rotor2)
 
-def num(letter):
-    if letter >= 'A' and letter <= 'Z':
-        return ord(letter) - ord('A')
-    else:
-        return ord(letter) - ord('a')
+    Reflector = Rotor(reflectors[reflectorName]["wiring"], 0, " ", None)
 
-Rotor1 = Rotor(wiring1, 6, "Q", None)
-Rotor2 = Rotor(wiring2, 3, "Q", Rotor1)
-Rotor3 = Rotor(wiring3, 7, "Q", Rotor2)
-Reflector = Rotor(reflect, 0, "Q", None)
 
-text = "We will advance tomorrow. Heil Shitler"
+text = "the quick brown fox jumped over the lazy dog the quick brown fox jumped over the lazy dog"
 output = ""
 n = 0
 
